fe1a4d85ecca999dc6d28b32d768058bfe86247f
Dymitr Wierszylowski <dymitr.werszylowski@gmail.com>
2024-08-20 15:33:05 +0200

    commit

--- app/lib/definitionProduct.ts
+++ app/lib/definitionProduct.ts
@@ -1,7 +1,27 @@
 export type Product = {
-    id: string;
+    id: string; //sku
+    category_id: string; //spozywka mototryzacja budownictwo
+    type_id: string; //smar spray klej silikon
+    provider_id: string; //molykote krytox cx80 rocol
     name: string;
     image_url: string;
     price: number;
     amount: number;
+    date: string; //20-08-2000 10:33
+    status:'on'|'off';
 };
+
+export type LatestProduct = Pick<Product, 'id'|'name'|'image_url'|'price'|'date'|'amount'>
+
+export type AmountOfProducts = Omit<Product, 'amount'> & {
+    amount: number;
+};
+
+export type ProductTable = {
+    id: string;
+    name: string; 
+    image_url: string;
+    price: number;
+    amount: number;
+    date: string;
+}
